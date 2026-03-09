abc5376c4ae15709ef75ab8b6b0b14c7a1ab1708
Thushar Kulal <thushar@Thushars-MacBook-Air.local>
2026-03-09 18:58:22 +0530

    Associations and validations

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,14 @@
 class Match < ApplicationRecord
-  belongs_to :winner
-  belongs_to :loser
+  belongs_to :winner, class_name: "Player", counter_cache: :wins
+  belongs_to :loser, class_name: "Player", counter_cache: :losses
+
+  validate :players_must_be_different
+
+  private
+
+  def players_must_be_different
+    if winner_id.present? && winner_id == loser_id
+      errors.add(:base, "Winner and loser must be different players")
+    end
+  end
 end
